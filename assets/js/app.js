b7f9847d4b2d02b7d158783a94c33bcc9d1c6eda
Enol <51820585+enolal826@users.noreply.github.com>
2022-09-15 22:38:41 +0200

    Tag content within markdown files (#229)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 /** Custom scripts */
+function hideContentByLevel() {
+    for(const element of document.querySelectorAll('[data-content-level]')) {
+        const level = element.getAttribute('data-content-level')
+        const userLevel = getContentLevel()
+    
+        if (userLevel && !level.includes(userLevel)) {
+        element.style.display = "none";
+        }
+    }
+}
+
+hideContentByLevel()
