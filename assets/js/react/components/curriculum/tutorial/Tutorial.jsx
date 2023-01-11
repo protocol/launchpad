c58764d0c7518b5f80d543e2dcad9f84fe0a8fe4
Enol Álvarez <enol826@gmail.com>
2023-01-11 15:11:47 +0100

    WIP

--- assets/js/react/components/curriculum/tutorial/Tutorial.jsx
+++ assets/js/react/components/curriculum/tutorial/Tutorial.jsx
@@ -1,3 +1,37 @@
+import {renderComponent} from "../../../services/util";
+import {App} from "../../App";
+import React, {useEffect, useState} from "react";
+
 const Tutorial = () => {
 
+  const [showTutorial, setShowTutorial] = useState(false)
+
+  const showDynamicTutorial = e => {
+    const content = document.getElementById("page-content")
+    content.style.display = "none"
+    const data = e.detail
+    setShowTutorial(true)
+  }
+
+  useEffect(() => {
+    return window.addEventListener('showDynamicTutorial', showDynamicTutorial)
+  }, [])
+
+  return <>
+    {showTutorial && <div style={{width: '100%', height: '100%', zIndex: 10000}}>
+      jjjj
+    </div>}
+  </>
 }
+
+
+const fetchData = async () => {
+  const content = document.getElementById("page-content")
+  content.style.display = "none"
+
+  return {
+
+  }
+}
+
+renderComponent(<App component={Tutorial} fetchData={fetchData} />, "dynamic-tutorial")
