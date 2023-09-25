da9386d269ede304baab53381123645d7a074e27
walker <walker@walkPro>
2023-09-25 11:38:15 -0600

    Updated nav for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["content/*.md", "layouts/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pl-blue": colors.sky["600"],
+        "pl-dark-blue": "#002256",
+        "pl-gray": "#f6f7fa",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
